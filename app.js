cd374c200ab054c55dff2617d1427dfa9ed69cdd
JacobKnaack <jacobknck@gmail.com>
2023-03-10 08:17:45 -0800

    adds styling and loops

--- app.js
+++ app.js
@@ -2,24 +2,30 @@
 
 let wantsASundae = prompt('Who you like a sundae?').toLowerCase(); // be careful if you click "cancel"
 
+let iceCreamFlavors = ['vanilla', 'chocolate', 'strawberry', 'rock road', 'mint'];
+let iceCreamToppings = ['sprinkles', 'peanuts', 'mochi'];
+
 if (wantsASundae === 'yes') {
   // ask about ice cream and toppings
   alert('Great, take a look at our ice cream flavors');
 
   let iceCreamFlavor = prompt('What flavor of ice cream would you like?').toLowerCase();
 
-  if (iceCreamFlavor === 'Vanilla' || iceCreamFlavor === 'Strawberry' || iceCreamFlavor === 'Rocky Road') {
+  if (iceCreamFlavors.includes(iceCreamFlavor)) {
     alert('Great!  Choose from our list of toppings');
 
     // choose 3 toppings
-    let toppingOne = prompt('What is you first topping?');
-    alert(toppingOne + ' Great! what else?');
-    let toppingTwo = prompt('What is you second topping?');
-    alert(toppingOne + ' ' + toppingTwo + ' Great! what else?');
-    let toppingThree = prompt('What is you third topping?');
-    alert(toppingOne + ' ' + toppingTwo + ' ' + toppingThree + ' Great!');
+    let toppingOne = prompt('What is you first topping?').toLowerCase();
 
-    // checking if toppings are valid
+    if (iceCreamToppings.includes(toppingOne)) {
+      alert(toppingOne +'Great! what else?');
+      let toppingTwo = prompt('What is you second topping?');
+      alert(toppingOne +'' + toppingTwo +'Great! what else?');
+      let toppingThree = prompt('What is you third topping?');
+      alert(toppingOne +'' + toppingTwo +'' + toppingThree +'Great!');
+
+      // checking if toppings are valid
+    }
 
   } else {
     alert('Ugh oh, we don\'t have that');
